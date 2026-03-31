ecdc345938aeae5aaf90b60db43fc396b3b78a84
Anderson Oliveira de Queiroz <sonyanderson19@gmail.com>
2026-03-31 22:20:49 +0100

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const map1 = new Map();
+        for(let i = 0;i < nums.length;i++){
+            if(map1.has(target - nums[i])){
+                return [map1.get(target - nums[i]), i]
+            }
+            map1.set(nums[i], i);
+        }
+    }
+}
