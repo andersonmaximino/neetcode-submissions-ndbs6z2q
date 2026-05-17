652c1f62857821a8b33050957bf81ae93b72b024
Anderson Oliveira de Queiroz <sonyanderson19@gmail.com>
2026-05-17 15:57:51 +0100

    Add: find-target-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while(left < right){
+        let mid = Math.floor((left + right) / 2);
+        if(nums[mid] < nums[right]){
+            right = mid;
+        }else{
+            left = mid + 1;
+        }
+    }
+    let min = left;
+
+    left = 0;
+    right = nums.length - 1;
+    if(target >= nums[min] && target <= nums[right]){
+        left = min;
+    }else{
+        right = min;
+    }
+
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2);
+        if(nums[mid] == target){
+            return mid;
+        }else if(nums[mid] < target){
+            left = mid + 1;
+        }else{
+            right = mid - 1;
+        }
+    }
+    return -1;       
+    }
+}
